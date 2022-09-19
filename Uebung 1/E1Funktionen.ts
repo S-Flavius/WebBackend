38ff5b27a3c2c297f1fbcf9a541bbb786eaa12a1
fsecheli <Flavius.Secheli@edvschule-plattling.de>
2022-09-19 10:37:39 +0200

    fix: check for division function feat: added function power()

--- Uebung 1/E1Funktionen.ts
+++ Uebung 1/E1Funktionen.ts
@@ -1,8 +1,10 @@
 pyramid(5, "-");
 pyramid(5);
+console.log(power(5, 4));
 
 const multiply = (...numbers: number[]) => numbers.reduce((a, b) => a * b);
 let product = multiply;
+
 console.log(product(5, 10, 5));
 console.log(division(10, 10, 5));
 
@@ -14,5 +16,16 @@ function pyramid(height: number, symbol: string = "*") {
 }
 
 function division(...numbers: number[]) {
-    return numbers.shift() ?? 1 / multiply(numbers);
+    if (numbers.length > 2) {
+        return numbers.shift() ?? 1 / multiply(...numbers);
+    }
+    return Math.min;
+}
+
+function power(base: number, exponent: number): number {
+    if (exponent != 1) {
+        return power(base, exponent - 1) * base;
+    } else {
+        return base;
+    }
 }
