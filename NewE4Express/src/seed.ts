cc199ef36976cb7892e2ed251f73a212e75bc9cd
fsecheli <Flavius.Secheli@edvschule-plattling.de>
2022-11-08 10:28:18 +0100

    Finished work with JWT

--- NewE4Express/src/seed.ts
+++ NewE4Express/src/seed.ts
@@ -1,13 +1,13 @@
 import { Task } from "./classes/Task";
-import { taskStore } from "./stores/TaskStore";
+import { userTaskStore } from "./stores/TaskStore";
 import { userStore } from "./stores/UserStore";
 import bcrypt from "bcrypt";
 
 
 export function seed() {
     let kehren = new Task("Kehren");
-    taskStore.set(kehren.uuid, kehren);
-    taskStore.add(new Task("Lernen", new Date("2022-09-26T07:58:30"), true));
+    userTaskStore.get("l@f.de").add(kehren);
+    userTaskStore.get("l@f.de").add(new Task("Lernen", new Date("2022 - 09 - 26T07: 58: 30"), true));
 
     let user = {
         "firstname": "Ludwig",
